133f79c9a182cdf44654c60e78d5e97cc80b8415
Yuri-ananin <ananin.yurij@mail.ru>
2025-02-06 02:26:15 +0700

    2 features(Contacts): добавлена реализация классов
    Добавлены классы Contact, ContactSerializer, JsonCommands, MainVM. Написаны комментарии. Сверстано главное окно. ЛР2 готова к защите.

---
+++ src/Contacts/View/ViewModel/JsonCommands.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace View.ViewModel
+{
+    /// <summary>
+    /// Команды для сохранения и загрузки данных. <see cref="ICommand"/>.
+    /// </summary>
+    class JsonCommands : ICommand
+    {
+        /// <summary>
+        /// Делегат <see cref="Action"/>.
+        /// </summary>
+        private readonly Action<object> _executeAction;
+
+        /// <summary>
+        /// Вызов действия.
+        /// </summary>
+        /// <param name="parameter">Параметр команды</param>.
+        public void Execute(object parameter) => _executeAction(parameter);
+
+        /// <summary>
+        /// Устанавливает возможность выполнения команды.
+        /// </summary>
+        /// <param name="parameter">Параметр команды.</param>
+        /// <returns>True - может. False - не может.</returns>
+        public bool CanExecute(object? parameter) => true;
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="JsonCommands"/> для выполнения действия.
+        /// </summary>
+        /// <param name="executeAction">Действие, которое будет выполнено.</param>
+        public JsonCommands(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+        }
+
+        /// <summary>
+        /// Событие, вызываемое при изменении условия на выполнение команды.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+    }
+}
