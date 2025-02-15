861db311d7e3ad0f3b42afe8beef80599c026604
Yuri-ananin <ananin.yurij@mail.ru>
2025-02-15 17:09:14 +0700

    3 features(ContactsMVVM): Добавлена реализация классов, ЛР3 готова к защите

---
+++ src/ContactsMVVM/ContactsMVVM/Model/Contact.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ContactsMVVM.Model
+{
+    /// <summary>
+    /// Хранит контактные данные телефона.
+    /// </summary>
+    internal class Contact : ICloneable
+    {
+
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает номер контакта.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Возвращает и задаёт почту контакта.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="number">Номер телефона.</param>
+        /// <param name="email">Адрес электронной почты.</param>
+        public Contact(string name, string number, string email)
+        {
+            Name = name;
+            PhoneNumber = number;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Создаёт пустой экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        public Contact()
+        {
+
+        }
+
+        /// <summary>
+        /// Создаёт копию контакта.
+        /// </summary>
+        /// <returns>Копия контакта.</returns>
+        public object Clone()
+        {
+            return new Contact(string.Copy(Name), string.Copy(PhoneNumber), string.Copy(Email));
+        }
+    }
+}
---
+++ src/ContactsMVVM/ContactsMVVM/Model/Services/ContactSerializer.cs
@@ -0,0 +1,56 @@
+﻿using ContactsMVVM.ViewModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace ContactsMVVM.Model.Services
+{
+    /// <summary>
+    /// Класс, предоставляющий методы для сериализации и десериализации списка контактов.
+    /// </summary>
+    internal static class ContactSerializer
+    {
+        /// <summary>
+        /// Путь к папке, где хранятся данные.
+        /// </summary>
+        private readonly static string _folderPath = Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.MyDocuments), "ContactsMVVM");
+
+        /// <summary>
+        /// Полный путь к файлу JSON, содержащему список контактов.
+        /// </summary>
+        private static readonly string _filePath = Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.MyDocuments), "ContactsMVVM", "contacts.json");
+
+
+        /// <summary>
+        /// Сохраняет коллекцию контактов в JSON-файл.
+        /// </summary>
+        /// <param name="contacts">Коллекция контактов <see cref="ObservableCollection{ContactVM}"/></param>
+        public static void SaveContacts(ObservableCollection<ContactVM> contacts)
+        {
+            if (!Directory.Exists(_folderPath))
+            {
+                Directory.CreateDirectory(_folderPath);
+            }
+            string json = JsonConvert.SerializeObject(contacts);
+            File.WriteAllText(_filePath, json);
+        }
+
+        /// <summary>
+        /// Загружает список контактов из JSON-файла.
+        /// </summary>
+        /// <returns>Если найден файл, то вернёт список контактов <see cref="ObservableCollection{ContactVM}"/>, 
+        /// иначе вернёт пустой список контактов <see cref="ObservableCollection{ContactVM}"/></returns>
+        public static ObservableCollection<ContactVM> LoadContacts()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new ObservableCollection<ContactVM>();
+            }
+            string jsonData = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<ObservableCollection<ContactVM>>(jsonData);
+        }
+    }
+}
